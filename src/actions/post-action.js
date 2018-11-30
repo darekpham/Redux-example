212f206d77aa8cf33cc9f6cf93ceb40950c41b66
Anh Dung Pham <a.pham@samsung.com>
2018-11-30 18:07:57 +0100

    add new post action

--- src/actions/post-action.js
+++ src/actions/post-action.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS, NEW_POST } from '../actions/types';
+import { FETCH_POSTS, NEW_POST} from '../actions/types';
 
 export const fetchPosts = () => dispatch => {
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -6,5 +6,21 @@ export const fetchPosts = () => dispatch => {
     .then (posts => dispatch({
         type: FETCH_POSTS,
         payload: posts
+    }));
+}
+
+
+export const createPost = (postData) => dispatch => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+    })
+    .then(res => res.json())
+    .then(post => dispatch({
+        type: NEW_POST,
+        payload: post
     }));
 }
--- src/components/posts-component.js
+++ src/components/posts-component.js
@@ -6,7 +6,14 @@ import { fetchPosts } from '../actions/post-action';
 class Posts extends Component {
 
     componentWillMount() {
+        // Call action
         this.props.fetchPosts();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.newPost) {
+            this.props.posts.unshift(nextProps.newPost);
+        }
     }
 
     render() {
@@ -28,11 +35,13 @@ class Posts extends Component {
 
 Posts.propTypes = {
     fetchPosts: PropTypes.func.isRequired,
-    posts: PropTypes.array.isRequired
+    posts: PropTypes.array.isRequired,
+    newPost: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
-    posts: state.posts.items
+    posts: state.posts.items,
+    newPost: state.posts.item
 })
 
 export default connect(mapStateToProps, { fetchPosts })(Posts);
