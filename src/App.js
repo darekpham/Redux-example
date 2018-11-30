b263d5c4ca825562d46b2a0cb4a0716f7874897a
Anh Dung Pham <a.pham@samsung.com>
2018-11-30 15:24:54 +0100

    created post-form-component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Components
 import Posts from './components/posts-component';
+import PostForm from './components/post-form-component';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <PostForm />
         <Posts />
       </div>
     );
